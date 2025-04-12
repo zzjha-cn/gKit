0c7d170982811990a52a7852043e9a241dd99b20
Sean Zeng <zengjinghao_em@163.com>
2025-04-12 13:55:32 +0800

    Update: 重构

---
+++ internal/notes/channel/channel_leak_test.go
@@ -0,0 +1,118 @@
+package channel
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"runtime"
+	"testing"
+	"time"
+)
+
+func TestExam(t *testing.T) {
+	// 情况一  输出是什么呢？
+	TestLeakOfMemory1(t)
+	// out:
+	// NumGoroutine: 2
+	// 超时
+	// NumGoroutine: 3
+	// 这里面一个协程没有退出，一直阻塞，并且没有回收，如果在循环中使用这样的代码，会吃内存，容易OOM
+
+	// 但是上面的情况即使加上了缓存，还要求收发的数量一样，不然，还是会内存泄漏
+	// 出现了第二种情况
+	fmt.Println("NumGoroutine:", runtime.NumGoroutine())
+	chanLeakOfMemory2()
+	time.Sleep(time.Second * 3) // 等待 goroutine 执行，防止过早输出结果
+	fmt.Println("NumGoroutine:", runtime.NumGoroutine())
+	// 就是超时机制，接收者这边直接离开了协程，剩下的生产者还是在阻塞中
+
+	// 所以，解决方式还是应该使用优雅的方式——增加一个额外的stop channel用来终结发送者的chan
+	chanLeakOfMemory3()
+}
+
+func TestLeakOfMemory1(t *testing.T) {
+	fmt.Println("NumGoroutine:", runtime.NumGoroutine())
+	chanLeakOfMemory()
+	time.Sleep(time.Second * 3) // 等待 goroutine 执行，防止过早输出结果
+	fmt.Println("NumGoroutine:", runtime.NumGoroutine())
+}
+
+func chanLeakOfMemory() {
+	errCh := make(chan error) // (1)
+	go func() {               // (5)
+		time.Sleep(2 * time.Second)
+		errCh <- errors.New("chan error") // (2)  这个就会一直阻塞着，不会回收，也没有东西可以结束阻塞
+	}()
+
+	var err error
+	select {
+	case <-time.After(time.Second): // (3) 大家也经常在这里使用 <-ctx.Done()
+		fmt.Println("超时")
+	case err = <-errCh: // (4)  这里有执行之后，（2）才能够执行，但是，上面会直接超时跳过
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(nil)
+		}
+	}
+} // 所以最终的原因也就是没有带缓冲的chan--除非增加有缓存的
+
+// ====================================================
+
+func chanLeakOfMemory2() {
+	ich := make(chan int, 100) // (3)
+	// sender
+	go func() {
+		defer close(ich)
+		for i := 0; i < 10000; i++ {
+			ich <- i
+			time.Sleep(time.Millisecond) // 控制一下，别发太快
+		}
+	}()
+	// receiver
+	go func() {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+		for i := range ich { // (2)
+			if ctx.Err() != nil { // (1)
+				fmt.Println(ctx.Err())
+				return // 这里你拍拍屁股直接退出了，然后生产者那边还是傻乎乎发送呢
+			}
+			fmt.Println(i)
+		}
+	}()
+} // 就是超时机制，接收者这边直接离开了协程，剩下的生产者还是在阻塞中
+
+// ====================================================
+func chanLeakOfMemory3() {
+	ich := make(chan int, 100) // (3)
+	tostop := make(chan struct{})
+
+	// sender
+	go func() {
+		defer close(ich)
+		for i := 0; i < 10000; i++ {
+			// ich <- i
+			select {
+			case <-tostop:
+				close(ich)
+				return
+			case ich <- i:
+			}
+			time.Sleep(time.Millisecond) // 控制一下，别发太快
+		}
+	}()
+	// receiver
+	go func() {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+		for i := range ich { // (2)
+			if ctx.Err() != nil { // (1)
+				fmt.Println(ctx.Err())
+				close(tostop)
+				return // 这里直接退出了，生产者那边却还发送
+			}
+			fmt.Println(i)
+		}
+	}()
+} // 就是超时机制
--- pkg/chain/chain_test.go
+++ pkg/chain/chain_test.go
@@ -7,86 +7,45 @@ import (
 )
 
 type (
-	ChainInterface interface {
-		MakeChainCtx(v any) *ChainContext
-		Next(ctx *ChainContext, ind int)
-	}
-
-	FilterHandle func(ctx *ChainContext)
-
-	FilterChain struct {
-		chain  []FilterHandle
-		before []int
-		after  []int
-
-		makeArg func(ctx ChainContext, args []reflect.Value) error
-		makeVal func(ctx ChainContext, args []reflect.Value) error
-	}
-
-	ChainContext struct {
-		MethodName string
-		Arg        []any
-		Val        []any
-		chain      []FilterHandle
-	}
-
 	Server struct {
+		Id string
 	}
 )
 
-func CombineMdChain[T any](fil *FilterChain, t T) T {
-	// t 传入的是要加中间件的服务方法
-	// 会将t动态代理，更改为带着上下文与前置后置逻辑的函数，然后返回
-
-	// 获取函数的反射值
-	fnValue := reflect.ValueOf(t)
-	typ := reflect.TypeOf(t)
-	if typ.Kind() != reflect.Func {
-		return t
-	}
-
-	// 创建一个新的函数
-	copiedFunc := reflect.MakeFunc(fnValue.Type(), func(args []reflect.Value) []reflect.Value {
-
-		// 调用原始函数
-		return fnValue.Call(args)
-	})
-
-	return copiedFunc.Interface().(T)
+func (s *Server) GetId(name string) string {
+	fmt.Println("[GetId]", name)
+	return s.Id
 }
 
 func TestUseMiddleware(t *testing.T) {
 	s := &Server{}
-	d := CombineMdChain(&FilterChain{}, s.GetId)
+	d := CombineSrvChain(&FilterChain{}, s.GetId)
 	id := d("name")
 	fmt.Println(id)
 }
 
-func TestObj(t *testing.T) {
-	s := &Server{}
+// 测试传递结构体而非传递方法
+func TestObject(t *testing.T) {
+	s := &Server{
+		Id: "id2",
+	}
 
 	fnv := reflect.ValueOf(s).Method(0)
 	t1 := reflect.TypeOf(s).Method(0).Type
 	t2 := fnv.Type()
-	fmt.Println(t1 == t2)
-	fmt.Println(t1.String())
-	fmt.Println(t2.String())
-	fmt.Println(fnv.Type().String())
+	fmt.Println("结构体方法类型与值类型是否相等：", t1 == t2)
+	fmt.Println("方法类型", t1.String())
+	fmt.Println("值类型", t2.String())
 
+	fmt.Println("值调用")
 	v := reflect.ValueOf("666")
+	valRes := fnv.Call([]reflect.Value{v})
+	fmt.Println(valRes[0].Interface())
 
-	re := fnv.Call([]reflect.Value{v})
-	fmt.Println(re[0].Interface())
-
+	fmt.Println("用值的类型创建函数调用")
 	cfn := reflect.MakeFunc(fnv.Type(), func(args []reflect.Value) (results []reflect.Value) {
 		return fnv.Call(args)
 	})
-
-	re = cfn.Call([]reflect.Value{v})
-	fmt.Println(re[0].Interface())
-}
-
-func (s *Server) GetId(name string) string {
-	fmt.Println("srv", name)
-	return "asds"
+	valRes = cfn.Call([]reflect.Value{v})
+	fmt.Println(valRes[0].Interface())
 }
