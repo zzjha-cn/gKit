b73d00198f6f6a235d5fb667ee88f357eb148da0
Sean Z <2654178379@qq.com>
2025-11-17 16:10:12 +0800

    Update: 增加stm的测试

--- internal/srv/etcd101/advance_test.go
+++ internal/srv/etcd101/advance_test.go
@@ -1,7 +1,11 @@
 package etcd101
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"math/rand"
+	"sync"
 	"testing"
 	"time"
 
@@ -73,4 +77,77 @@ func TestBarrier(t *testing.T) {
 			fmt.Println("结束", ind)
 		}(i)
 	}
+}
+
+func TestSTM(t *testing.T) {
+	cfg := clientv3.Config{
+		Endpoints: []string{"http://127.0.0.1:2379"},
+	}
+
+	etcdClient, _ := clientv3.New(cfg)
+	defer etcdClient.Close()
+
+	// 批量操作多个key与执行多个任务
+
+	totalAccounts := 5
+	for i := 0; i < totalAccounts; i++ {
+		k := fmt.Sprintf("accts/%d", i)
+		if _, err := etcdClient.Put(context.TODO(), k, "100"); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	// 核心逻辑(批量动作)
+	exchange := func(stm concurrency.STM) error {
+		from := rand.Intn(5)
+		to := rand.Intn(5)
+		if from == to {
+			return nil
+		}
+		fromK, toK := fmt.Sprintf("accts/%d", from), fmt.Sprintf("accts/%d", to)
+		fromV, toV := stm.Get(), stm.Get(fmt.Sprintf("accts/%d", to))
+		fromInt, toInt := 0, 0
+		fmt.Sscanf(fromV, "%d", &fromInt)
+		fmt.Sscanf(toV, "%d", &toInt)
+
+		// 把源账号一半的钱转账给目标账号
+		xfer := fromInt / 2
+		fromInt, toInt = fromInt-xfer, toInt+xfer
+		// 把转账后的值写回
+		stm.Put(fromK, fmt.Sprintf("%d", fromInt))
+		stm.Put(toK, fmt.Sprintf("%d", toInt))
+		return nil
+	}
+
+	// 并发执行
+	var wg = sync.WaitGroup{}
+	for i := 0; i < 20; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			_, err := concurrency.NewSTM(etcdClient, exchange)
+			if err != nil {
+				fmt.Println(err)
+			}
+		}()
+	}
+
+	wg.Wait()
+
+	// 验证总量
+	var sum = 0
+	// for i := 0; i < totalAccounts; i++ {
+	// 	val, err := etcdClient.Get(context.TODO(), fmt.Sprintf("accts/%d", i))
+	// 	if nil != err {
+	// 		continue
+	// 	}
+	// 	val.Kvs[0].Value
+	// }
+	res, _ := etcdClient.Get(context.TODO(), "accts/", clientv3.WithPrefix())
+	for _, kv := range res.Kvs {
+		v := 0
+		fmt.Sscanf(string(kv.Value), "%d", &v)
+		sum += v
+	}
+	fmt.Println(sum)
 }
